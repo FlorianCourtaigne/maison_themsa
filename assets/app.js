08763d9a2dd91faec5ee74517ec4ee3b67de9ff5
FlorianCourtaigne <f.courtaigne@gmail.com>
2022-09-25 12:55:02 +0200

    working on navbar

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,16 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+console.log('Hello Webpack Encore !')
+
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
